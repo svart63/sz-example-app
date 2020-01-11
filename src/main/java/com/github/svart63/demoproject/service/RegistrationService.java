bb00beadeac43144ac58ef5796bc4f857df36a48
Salavat R <svart63@gmail.com>
2020-01-11 18:06:32 +0400

    changed injection to setter

--- src/main/java/com/github/svart63/demoproject/service/RegistrationService.java
+++ src/main/java/com/github/svart63/demoproject/service/RegistrationService.java
@@ -11,7 +11,7 @@ import java.util.regex.Pattern;
 
 @Service
 public class RegistrationService extends AuthService<RegistrationRepository> {
-    private final BCryptPasswordEncoder encoder;
+    private BCryptPasswordEncoder encoder;
     private Pattern emailPattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+" +
             "(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"" +
             "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])" +
@@ -21,8 +21,12 @@ public class RegistrationService extends AuthService<RegistrationRepository> {
             "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])");
 
     @Autowired
-    public RegistrationService(RegistrationRepository repo, BCryptPasswordEncoder encoder) {
+    public RegistrationService(RegistrationRepository repo) {
         super(repo);
+    }
+
+    @Autowired
+    public void setEncoder(BCryptPasswordEncoder encoder) {
         this.encoder = encoder;
     }
 
